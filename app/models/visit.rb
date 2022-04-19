fc6cff2ee417430f294155788b2ff0e3559e5256
Andrew Garshyn <andrew.garshyn@sumup.com>
2022-04-19 15:54:39 +0200

    register a visit with unit test

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,2 +1,8 @@
 class Visit < ApplicationRecord
+  def self.register(url, ip)
+    visit = find_or_initialize_by url: url, ip: ip
+    visit.number += 1
+    visit.save!
+    visit
+  end
 end
---
+++ spec/models/visit_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Visit, type: :model do
+  describe '.register' do
+    subject { described_class.register(path, ip) }
+
+    let(:path) { '/' }
+    let(:ip) { '1.1.1.1' }
+
+    context 'when new visit' do
+      it 'creates a new visit' do
+        expect { subject }.to change(described_class, :count).by(1)
+
+        expect(Visit.last).to have_attributes(
+          url: path,
+          ip: ip,
+          number: 1,
+        )
+      end
+    end
+
+    context 'when returned visit' do
+      before { described_class.create! url: path, ip: ip, number: 1 }
+
+      it 'increments a visit' do
+        expect { subject }.to change(described_class, :count).by(0)
+
+        expect(Visit.last).to have_attributes(
+          url: path,
+          ip: ip,
+          number: 2,
+        )
+      end
+    end
+  end
+end

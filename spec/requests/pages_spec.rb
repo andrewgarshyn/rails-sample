a4ed8497c316702a69cef7b11449cb31fd6b4e2a
Andrew Garshyn <andrew.garshyn@sumup.com>
2022-07-22 15:51:10 +0200

    fix a test

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -4,9 +4,7 @@ RSpec.describe "Home", type: :request do
   describe "GET pages/home" do
     it "schedules a job" do
       ActiveJob::Base.queue_adapter = :test
-      expect {
-        get "/"
-      }.to have_enqueued_job(RegisterVisitJob) #.with("/", "?")
+      expect { get "/" }.to have_enqueued_job(RegisterVisitJob).with("/", "127.0.0.1")
     end
   end
 end

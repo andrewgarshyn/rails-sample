572d9a8b1a88637b36c5aed9b5c7307021ddb380
Andrew Garshyn <andrew.garshyn@sumup.com>
2022-07-22 15:48:37 +0200

    install rubocop rspec

--- spec/models/visit_spec.rb
+++ spec/models/visit_spec.rb
@@ -2,16 +2,16 @@ require "rails_helper"
 
 RSpec.describe Visit, type: :model do
   describe ".register" do
-    subject { described_class.register(path, ip) }
+    subject(:register_visit) { described_class.register(path, ip) }
 
     let(:path) { "/" }
     let(:ip) { "1.1.1.1" }
 
     context "when new visit" do
       it "creates a new visit" do
-        expect { subject }.to change(described_class, :count).by(1)
+        expect { register_visit }.to change(described_class, :count).by(1)
 
-        expect(Visit.last).to have_attributes(
+        expect(described_class.last).to have_attributes(
           url: path,
           ip:,
           number: 1,
@@ -23,9 +23,9 @@ RSpec.describe Visit, type: :model do
       before { described_class.create! url: path, ip:, number: 1 }
 
       it "increments a visit" do
-        expect { subject }.to change(described_class, :count).by(0)
+        expect { register_visit }.not_to change(described_class, :count)
 
-        expect(Visit.last).to have_attributes(
+        expect(described_class.last).to have_attributes(
           url: path,
           ip:,
           number: 2,
